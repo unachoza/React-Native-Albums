fc347c8faac763628651fa31482c1ea070995e43
Arianna Choza <unachoza@gmail.com>
2019-07-25 21:24:18 -0400

    button styles opacity user input

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,35 @@
 import React from 'react'
-import {Text} from 'react-native'
+import {Text, TouchableOpacity} from 'react-native'
 
 const Button = () => {
+    const {buttonStyle, textStyle} = style
     return (
-        <Text>Click Me</Text>
+        <TouchableOpacity style={buttonStyle}>
+            <Text style={textStyle}>Click Me</Text>
+        </TouchableOpacity>
     )
+}
+
+const style = {
+    textStyle: {
+        alignSelf: 'center', 
+        color: '#007aff', 
+        fontSize: 16, 
+        paddingTop: 10, 
+        paddingBottom: 10,
+        fontWeight: '600'
+    },
+    buttonStyle: {
+        flex: 1, 
+        alignSelf: 'stretch', 
+        borderColor: '#007aff',
+        backgroundColor: '#fff',
+        borderRadius: 5, 
+        borderWidth: 1,
+        marginLeft: 5,
+        marginRight: 5
+
+    }
 }
 
 export default Button
